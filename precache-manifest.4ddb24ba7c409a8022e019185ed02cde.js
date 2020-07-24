b025c59eca35a0e345f40df9de1edfeb2222603a
Kartik <40801023+kartik4all@users.noreply.github.com>
2020-07-25 01:39:05 +0530

    done

---
+++ precache-manifest.4ddb24ba7c409a8022e019185ed02cde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c980b0051762c98b3a2b1d6268a3c9b",
+    "url": "/venueMonk2/index.html"
+  },
+  {
+    "revision": "b35d9a07dc98029ff4a6",
+    "url": "/venueMonk2/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c2a050d7e289d3522aa6",
+    "url": "/venueMonk2/static/css/main.2ffbf689.chunk.css"
+  },
+  {
+    "revision": "b35d9a07dc98029ff4a6",
+    "url": "/venueMonk2/static/js/2.65914fa5.chunk.js"
+  },
+  {
+    "revision": "31a3bb157dfe3a3d9b33c9ab8def895f",
+    "url": "/venueMonk2/static/js/2.65914fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a050d7e289d3522aa6",
+    "url": "/venueMonk2/static/js/main.90d1fc51.chunk.js"
+  },
+  {
+    "revision": "13b42b7aa09e27f38cb6",
+    "url": "/venueMonk2/static/js/runtime-main.f8b75fad.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/venueMonk2/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
